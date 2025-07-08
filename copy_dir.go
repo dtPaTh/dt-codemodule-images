6524843e676653c0779f9752d535d2a969521722
Patrick <dtPaTh@users.noreply.github.com>
2025-07-08 17:23:32 +0200

    Update copy_dir.go

--- copy_dir.go
+++ copy_dir.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "io"
     "os"
+    "os/exec"
     "path/filepath"
     "time"
 )
@@ -41,13 +42,34 @@ func main() {
     fmt.Println("Running copy_dir version v0.4")
 
     if len(os.Args) < 3 {
-        fmt.Println("Usage: copy_dir <source_directory> <target_directory> [keepalive]")
+        fmt.Println("Usage: copy_dir <source_directory> <target_directory> [keepalive] [<bootstrap-command>] ")
         return
     }
 
     sourceDir := os.Args[1]
     destinationDir := os.Args[2]
 
+    nextArg = 3
+
+    keepAlive := false
+    
+    if len(os.Args) >= (nextArg+1) && os.Args[nextArg] == "keepalive" {
+        keepAlive = true
+        nextArg++
+    }
+    
+    if len(os.Args) >= (nextArg+1) {
+        executable := os.Args[nextArg]   
+	    args := os.Args[(nextArg+1):]   
+    	cmd := exec.Command(os.Args[nextArg], os.Args[(nextArg+1):]...)
+    	cmd.Stdout = os.Stdout
+    	cmd.Stderr = os.Stderr
+
+        fmt.Printf("Bootstrapping: %s %v\n", executable, args)
+        if err := cmd.Run(); err != nil {
+		    fmt.Fprintf(os.Stderr, "Execution failed: %v\n", err)
+	    }
+    }
     _, err := os.Stat(destinationDir)
     if os.IsNotExist(err) {    
         err := copyDir(sourceDir, destinationDir)
@@ -60,7 +82,7 @@ func main() {
         fmt.Println("Sourcedirectory already exists. Skip copying!")
     }
     
-    if len(os.Args) >= 4 && os.Args[3] == "keepalive" {
+    if keepAlive {
         fmt.Println("keepalive...")
         for { 
             time.Sleep(time.Hour)
